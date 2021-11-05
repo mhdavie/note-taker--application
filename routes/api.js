c98d5b18e6fab5ab83e0ce4b01d5cc71cd309bbb
mdavie <marianahodniki1991@gmail.com>
2021-11-04 21:38:35 -0600

    added get, post request on the api file

--- routes/api.js
+++ routes/api.js
@@ -12,4 +12,20 @@ router.get('/notes', (req, res) => {
 
 //post
 
+router.post("/notes",  (req, res) =>{
+    storage
+    .addNote()(req.body)
+    .then((notes) => {
+        return res.json(notes)
+
+    }).catch(err => res.status(500).json(err))
+});
+
+
 // delete
+router.delete("/notes/:title", (req, res) => {
+    storage
+        .removeNote()(req.params.title)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+});
