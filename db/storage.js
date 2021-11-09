25e22127763e0082098d41be77b7b6f240d4cae3
mdavie <marianahodniki1991@gmail.com>
2021-11-09 14:09:33 -0700

    added path and added correct uuiv4 and fixed css relative path

--- db/storage.js
+++ db/storage.js
@@ -1,6 +1,6 @@
 const util = require('util');
 const fs = require('fs');
-const uuidv1 = require('uuid/v1')
+const { v1: uuidv1 } = require('uuid');
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile)
 
