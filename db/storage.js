c7cabe823fe621b90ce50e13ec8f3956b6397b7d
mdavie <marianahodniki1991@gmail.com>
2021-11-10 09:29:08 -0700

    latest repo

--- db/storage.js
+++ db/storage.js
@@ -14,31 +14,29 @@ class Storage {
         return writeFile('db/db.json', JSON.stringify(note))
         }
 
-    getNotes() {
-        return this.read().then((notes) => {
-            let parsedNotes;
-            try {
-                parsedNotes = [].concat(JSON.parse(notes));
-            }catch(err) {
-                parsedNotes=[];            }
-return parsedNotes
-        })
+    async getNotes() {
+        const notes = await this.read();
+        let parsedNotes;
+        try {
+            parsedNotes = [].concat(JSON.parse(notes));
+        } catch (err) {
+            parsedNotes = [];
+        }
+        return parsedNotes;
     }
-    addNote(note) {
+    async addNote(note) {
         const {title, text} = note;
         const newNote = {title, text, id: uuidv1() }
         // get all the notes
-        return this.getNotes()
-        //add the new notes
-        .then((notes) => [...notes, newNote])
-        // write all the updated notes
-        .then((changedNotes) => this.write(changedNotes))
-        // returning new note
-        .then(() => newNote)
-    } removeNote(id){
-        return this.getNotes()
-        .then((notes) => notes.filter((note) => note.id !== id))
-        .then((filteredNote) => this.write(filteredNote));
+        const notes = await this.getNotes();
+        const changedNotes = [...notes, newNote];
+        await this.write(changedNotes);
+        return newNote;
+        
+    } async removeNote(id){
+        const notes = await this.getNotes();
+        const filteredNote = notes.filter((note) => note.id !== id);
+        return await this.write(filteredNote);
         
     }
 
